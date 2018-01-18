0dd22a8d55eeddebfea99bb79ab620d0f404846d
Billy Donahue <BillyDonahue@users.noreply.github.com>
2018-01-18 15:41:17 -0500

    compins/contrast

--- Adafruit_SSD1306.cpp
+++ Adafruit_SSD1306.cpp
@@ -249,6 +249,19 @@ void Adafruit_SSD1306_Core::begin(uint8_t vccstate, uint8_t i2caddr, bool reset)
   }
 
   // Init sequence
+
+  int8_t compins_alt = 0;  // sequential / alternative#
+  bool extvcc = (vccstate == SSD1306_EXTERNALVCC);
+  int8_t contrast;
+  if (WIDTH == 128 && HEIGHT == 32) {
+    contrast = 0x8F;
+  } else if (WIDTH == 128 && HEIGHT == 64) {
+    compins_alt = 1;
+    contrast = extvcc ? 0x9F : 0xCF;
+  } else if (WIDTH == 96 && HEIGHT == 16) {
+    contrast = extvcc ? 0x10 : 0xAF;
+  }
+
   ssd1306_command(SSD1306_DISPLAYOFF);                    // 0xAE
   ssd1306_command(SSD1306_SETDISPLAYCLOCKDIV);            // 0xD5
   ssd1306_command(0x80);                                  // the suggested ratio 0x80
@@ -260,43 +273,17 @@ void Adafruit_SSD1306_Core::begin(uint8_t vccstate, uint8_t i2caddr, bool reset)
   ssd1306_command(0x0);                                   // no offset
   ssd1306_command(SSD1306_SETSTARTLINE | 0x0);            // line #0
   ssd1306_command(SSD1306_CHARGEPUMP);                    // 0x8D
-  if (vccstate == SSD1306_EXTERNALVCC)
-    { ssd1306_command(0x10); }
-  else
-    { ssd1306_command(0x14); }
+  ssd1306_command(extvcc ? 0x10 : 0x14);
   ssd1306_command(SSD1306_MEMORYMODE);                    // 0x20
   ssd1306_command(0x00);                                  // 0x0 act like ks0108
   ssd1306_command(SSD1306_SEGREMAP | 0x1);
   ssd1306_command(SSD1306_COMSCANDEC);
-
-  if (WIDTH == 128 && HEIGHT == 32) {
-    ssd1306_command(SSD1306_SETCOMPINS);                    // 0xDA
-    ssd1306_command(0x2);
-    ssd1306_command(SSD1306_SETCONTRAST);                   // 0x81
-    ssd1306_command(0x8F);
-  } else if (WIDTH == 128 && HEIGHT == 64) {
-    ssd1306_command(SSD1306_SETCOMPINS);                    // 0xDA
-    ssd1306_command(0x12);
-    ssd1306_command(SSD1306_SETCONTRAST);                   // 0x81
-    if (vccstate == SSD1306_EXTERNALVCC)
-      { ssd1306_command(0x9F); }
-    else
-      { ssd1306_command(0xCF); }
-  } else if (WIDTH == 96 && HEIGHT == 16) {
-    ssd1306_command(SSD1306_SETCOMPINS);                    // 0xDA
-    ssd1306_command(0x2);   //ada x12
-    ssd1306_command(SSD1306_SETCONTRAST);                   // 0x81
-    if (vccstate == SSD1306_EXTERNALVCC)
-      { ssd1306_command(0x10); }
-    else
-      { ssd1306_command(0xAF); }
-  }
-
+  ssd1306_command(SSD1306_SETCOMPINS);                    // 0xDA
+  ssd1306_command((compins_alt << 4) | 0x2);
+  ssd1306_command(SSD1306_SETCONTRAST);                   // 0x81
+  ssd1306_command(contrast);
   ssd1306_command(SSD1306_SETPRECHARGE);                  // 0xd9
-  if (vccstate == SSD1306_EXTERNALVCC)
-    { ssd1306_command(0x22); }
-  else
-    { ssd1306_command(0xF1); }
+  ssd1306_command(extvcc ? 0x22 : 0xF1);
   ssd1306_command(SSD1306_SETVCOMDETECT);                 // 0xDB
   ssd1306_command(0x40);
   ssd1306_command(SSD1306_DISPLAYALLON_RESUME);           // 0xA4
